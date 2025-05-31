30e138dcb54a4435e95bdd68da5be77d0a1ae0d8
Tassis Fernando <tsfernando72@gmail.com>
2025-05-31 17:49:53 -0300

    corrige builder

--- src/main/java/br/com/tassisf/bank/controller/exception/ErrorResponse.java
+++ src/main/java/br/com/tassisf/bank/controller/exception/ErrorResponse.java
@@ -1,7 +1,10 @@
 package br.com.tassisf.bank.controller.exception;
 
+import lombok.Builder;
+
 import java.time.LocalDateTime;
 
+@Builder
 public record ErrorResponse(Integer status, String message, String path,
                             LocalDateTime timestamp) {
 }
